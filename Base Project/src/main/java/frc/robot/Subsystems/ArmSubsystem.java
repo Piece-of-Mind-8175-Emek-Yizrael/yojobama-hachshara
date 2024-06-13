59c31129448c980b6cc4cea4edd89457e5d06736
Yoav John Barak-Maurice <yojobama@gmail.com>
2024-06-13 18:32:58 +0300

    adding ArmSubsystem

---
+++ Base Project/src/main/java/frc/robot/Subsystems/ArmSubsystem.java
@@ -0,0 +1,71 @@
+package frc.robot.Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ArmSubsystem extends SubsystemBase
+{
+    public enum ArmDirection{
+        OPEN,CLOSE,HOLD,FREE
+    }
+    public enum ArmPosition
+    {
+        HIGH(Constants.ArmConstants.ARM_ENCODER_POSITION_HIGH),MID(Constants.ArmConstants.ARM_ENCODER_POSITION_MIDE);
+
+        double position;
+
+        ArmPosition(double position) {this.position = position;}
+
+        public double getValue() {return position;}
+    }
+
+    DigitalInput ground,fold;
+    CANSparkMax liftMotor;
+
+    private RelativeEncoder encoder;
+    private ArmFeedforward ff;
+
+    ArmDirection direction;
+
+    public ArmSubsystem()
+    {
+        ff = new ArmFeedforward(0, Constants.ArmConstants.FF_KG , 0);
+        liftMotor = new CANSparkMax(Constants.ArmConstants.LIFT_MOTOR_PORT, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
+        ground = new DigitalInput(Constants.ArmConstants.GROUND_SWICH_PORT);
+        fold = new DigitalInput(Constants.ArmConstants.FOLD_SWICH_PORT);
+        encoder = liftMotor.getEncoder();
+    }
+
+    double resistGravity(){
+        return ff.calculate(encoder.getPosition(), 0);
+    }
+
+    void setSpeed(double speed)
+    {
+        liftMotor.set(speed);
+    }
+    public ArmDirection getDirection() {return direction;}
+
+    public Command open(double speed)
+    {
+        return Commands.startEnd(() -> setSpeed(speed + resistGravity()),() -> setSpeed(0),this).until(() -> !ground.get());
+    }
+
+    public Command close(double speed)
+    {
+        return Commands.startEnd(() -> setSpeed(speed + resistGravity()),() -> setSpeed(0),this).until(() -> !fold.get());
+    }
+
+    public Command goToPosition(double speed,ArmPosition position)
+    {
+        direction = encoder.getPosition() < position.getValue() ? ArmDirection.OPEN : ArmDirection.CLOSE;
+        return Commands.startEnd(() -> setSpeed(speed + resistGravity()),() -> setSpeed(resistGravity()),this)
+                .until(() -> (direction==ArmDirection.CLOSE ? encoder.getPosition()>position.getValue() : encoder.getPosition()<position.getValue()));
+    }
+}
